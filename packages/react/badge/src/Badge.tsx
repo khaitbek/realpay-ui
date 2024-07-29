5040b549942a6c404508b0f48168fcde3502cf76
khaitbek <khaitbek2005@gmail.com>
2024-07-29 16:13:16 +0500

    Update badge: add border

--- packages/react/badge/src/Badge.tsx
+++ packages/react/badge/src/Badge.tsx
@@ -16,8 +16,8 @@ const badgeVariants = cva(
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
         success:"border-transparent bg-success text-success-foreground hover: bg-sucess/80",
-        outlineDestructive:"text-destructive",
-        outlineSuccess:"text-success",
+        outlineDestructive:"text-destructive border-destructive",
+        outlineSuccess:"text-success border-success",
 
       },
     },
