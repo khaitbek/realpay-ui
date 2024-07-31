db66588759197a1e0159bfa219354275743472b6
khaitbek <khaitbek2005@gmail.com>
2024-07-31 15:35:53 +0500

    Fix outline & secondary styles of badge component

--- packages/react/badge/src/Badge.tsx
+++ packages/react/badge/src/Badge.tsx
@@ -11,10 +11,10 @@ const badgeVariants = cva(
         default:
           "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
         secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "border-transparent bg-secondary text-primary hover:bg-secondary/80",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground",
+        outline: "text-background",
         success:
           "border-transparent bg-success text-success-foreground hover: bg-sucess/80",
         outlineDestructive: "text-destructive border-destructive",
