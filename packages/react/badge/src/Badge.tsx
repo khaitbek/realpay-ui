33467aeba550d596baeb8cc7b6ee21176172aecf
khaitbek <khaitbek2005@gmail.com>
2024-07-31 15:04:28 +0500

    Add new styles to the badge component

--- packages/react/badge/src/Badge.tsx
+++ packages/react/badge/src/Badge.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { classnames as cn } from "@hayitbek/realpay-utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-4 py-3 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
@@ -19,18 +19,27 @@ const badgeVariants = cva(
           "border-transparent bg-success text-success-foreground hover: bg-sucess/80",
         outlineDestructive: "text-destructive border-destructive",
         outlineSuccess: "text-success border-success",
+        darkDestructive: "bg-[#FF00001A] text-white",
+      },
+      size: {
+        small: "px-2.5 py-0.5",
+        medium: "p-3",
       },
     },
     defaultVariants: {
       variant: "default",
+      size: "small",
     },
   },
 );
 
-export type BadgeProps<T extends React.ElementType> = {
-  as?: T;
-} & Omit<React.ComponentProps<T>, "as"> &
-  VariantProps<typeof badgeVariants>;
+export type BadgeProps<T extends React.ElementType> = Omit<
+  React.ComponentProps<T>,
+  "as"
+> &
+  VariantProps<typeof badgeVariants> & {
+    as?: T;
+  };
 
 const Badge = <T extends React.ElementType>(props: BadgeProps<T>) => {
   const { as: Comp = "div", className, variant } = props;
